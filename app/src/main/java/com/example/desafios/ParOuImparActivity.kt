11abff0aab2e1d95adf229921d0dc12e5944e321
Gabriel <gabrielcasanova328@gmail.com>
2019-04-14 15:40:02 -0300

    Btn Back on Activity ParOuÍmapar
    Signed-off-by: Gabriel <gabrielcasanova328@gmail.com>

--- app/src/main/java/com/example/desafios/ParOuImparActivity.kt
+++ app/src/main/java/com/example/desafios/ParOuImparActivity.kt
@@ -2,11 +2,21 @@ package com.example.desafios
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class ParOuImparActivity : AppCompatActivity() {
+class ParOuImparActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onClick(v: View) {
+        val id = v.id
+
+        when(id) {
+            R.id.btn_back -> onBackPressed()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_par_ou_impar)
+
+
     }
 }
