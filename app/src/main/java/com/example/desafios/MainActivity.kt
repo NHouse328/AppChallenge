e8e422cc33ad0e402f7cea171bef6a70cdc4eeed
Gabriel <gabrielcasanova328@gmail.com>
2019-04-14 15:04:15 -0300

    Create ParOuÍmpar Activity and Btn startActivity ParOuÍmpar
    Signed-off-by: Gabriel <gabrielcasanova328@gmail.com>

--- app/src/main/java/com/example/desafios/MainActivity.kt
+++ app/src/main/java/com/example/desafios/MainActivity.kt
@@ -1,9 +1,22 @@
 package com.example.desafios
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onClick(v: View) {
+        val id = v.id
+
+        when (id) {
+            R.id.btn_to_screen_par_ou_impar -> {
+                val intent = Intent(this, ParOuImparActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
