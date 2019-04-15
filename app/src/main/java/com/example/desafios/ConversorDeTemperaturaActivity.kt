6b4cc5538444331816eb96cffa3311d9313cad9c
Gabriel <gabrielcasanova328@gmail.com>
2019-04-14 21:16:07 -0300

    ConversorDeTemperatura Layout finished
    Signed-off-by: Gabriel <gabrielcasanova328@gmail.com>

--- app/src/main/java/com/example/desafios/ConversorDeTemperaturaActivity.kt
+++ app/src/main/java/com/example/desafios/ConversorDeTemperaturaActivity.kt
@@ -3,6 +3,7 @@ package com.example.desafios
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_conversor_de_temperatura.*
 
 class ConversorDeTemperaturaActivity : AppCompatActivity(), View.OnClickListener {
@@ -19,6 +20,12 @@ class ConversorDeTemperaturaActivity : AppCompatActivity(), View.OnClickListener
         setContentView(R.layout.activity_conversor_de_temperatura)
 
         vincular()
+
+        val itens = arrayListOf<String>("Grau Celsius (oC)","Graus Fahrenheit (oF)","Kelvin (K)")
+        val adapter = ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,itens)
+
+        spinner1.adapter = adapter
+        spinner2.adapter = adapter
     }
 
     private fun vincular() {
